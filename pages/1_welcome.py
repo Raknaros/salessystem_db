e4ef82443f7f5b31816e190a328e8fe8cb788439
Raknaros <giumarchand@gmail.com>
2024-07-21 09:07:39 -0500

    Fourth

---
+++ pages/1_welcome.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from Login import authenticator
+
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
+
+st.write("Hello ,let's learn how to build a streamlit app together")
+
+st.title("this is the app title")
+st.header("this is the markdown")
+st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+st.image("pages/media/testlogo.png",caption="testlogo")
+st.audio("Audio.mp3")
+st.video("video.mp4")
