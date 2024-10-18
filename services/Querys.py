5a795112e4c18cc54d723c8c2b0bfd8ee3356263
Raknaros <giumarchand@gmail.com>
2024-10-18 12:44:22 -0500

    Fourth

--- services/Querys.py
+++ services/Querys.py
@@ -1,13 +1,19 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import toml
+import streamlit as st
 
+salessystem_user = st.secrets['DB_USERNAME_SALESSYSTEM']
+salessystem_token = st.secrets['DB_TOKEN_SALESSYSTEM']
+warehouse_user = st.secrets['DB_USERNAME_WAREHOUSE']
+warehouse_token = st.secrets['DB_TOKEN_WAREHOUSE']
+salessystem = create_engine('mysql+pymysql://'+salessystem_user+'@'+salessystem_token+'@sales-system.c988owwqmmkd.us'
+                                                                                      '-east-1.rds.amazonaws.com'
+                                                                                      ':3306/salessystem')
 
-
-salessystem = create_engine('mysql+pymysql://admin:Giu72656770@sales-system.c988owwqmmkd.us-east-1.rds.amazonaws.com'
-                            ':3306/salessystem')
-
-warehouse = create_engine('postgresql://admindb:72656770@datawarehouse.cgvmexzrrsgs.us-east-1.rds.amazonaws.com'
-                          ':5432/warehouse')
+warehouse = create_engine('postgresql://'+warehouse_user+'@'+warehouse_token+'@datawarehouse.cgvmexzrrsgs.us-east-1'
+                                                                             '.rds.amazonaws.com'
+                                                                             ':5432/warehouse')
 
 pedidos = pd.read_sql("SELECT * FROM pedidos",
                       salessystem)
--- test.py
+++ test.py
@@ -12,11 +12,7 @@ from services.Querys import cargar_datos
 passwords_to_hash = ['emisor2024', 'EvelynCBM1968', 'p259E9C695+']
 #hashed_passwords = Hasher(passwords_to_hash).generate()
 
-#salessystem = create_engine('mysql+pymysql://admin:Giu72656770@sales-system.c988owwqmmkd.us-east-1.rds.amazonaws.com'
-#                            ':3306/salessystem')
 
-#cotizaciones_poremitir = pd.read_sql("SELECT * FROM facturas WHERE estado NOT IN ('TERMINADO', 'ENTREGADO', 'ANULADA')",
-#                                     salessystem)
 
 pedidos = pd.read_excel('D:/OneDrive/facturacion/importar.xlsx', sheet_name='pedidos', date_format='%d/%m/%Y',
                                 dtype={'periodo': np.int32, 'adquiriente': object, 'importe_total': np.int64,
