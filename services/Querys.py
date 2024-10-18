8f5f1c9242e4b1e65bc1194cee43e0f13f3dc36a
Raknaros <giumarchand@gmail.com>
2024-10-18 12:57:12 -0500

    Fourth

--- services/Querys.py
+++ services/Querys.py
@@ -7,13 +7,13 @@ salessystem_user = st.secrets['DB_USERNAME_SALESSYSTEM']
 salessystem_token = st.secrets['DB_TOKEN_SALESSYSTEM']
 warehouse_user = st.secrets['DB_USERNAME_WAREHOUSE']
 warehouse_token = st.secrets['DB_TOKEN_WAREHOUSE']
-salessystem = create_engine('mysql+pymysql://'+salessystem_user+':'+salessystem_token+'@sales-system.c988owwqmmkd.us'
-                                                                                      '-east-1.rds.amazonaws.com'
-                                                                                      ':3306/salessystem')
 
-warehouse = create_engine('postgresql://'+warehouse_user+':'+warehouse_token+'@datawarehouse.cgvmexzrrsgs.us-east-1'
-                                                                             '.rds.amazonaws.com'
-                                                                             ':5432/warehouse')
+salessystem = create_engine(
+    'mysql+pymysql://' + salessystem_user + ':' + salessystem_token + '@sales-system.c988owwqmmkd.us'
+                                                                      '-east-1.rds.amazonaws.com'
+                                                                      ':3306/salessystem')
+warehouse_url = f'postgresql://{warehouse_user}:{warehouse_token}@datawarehouse.cgvmexzrrsgs.us-east-1.rds.amazonaws.com:5432/warehouse'
+warehouse = create_engine(warehouse_url)
 
 pedidos = pd.read_sql("SELECT * FROM pedidos",
                       salessystem)
@@ -21,9 +21,10 @@ pedidos = pd.read_sql("SELECT * FROM pedidos",
 
 def cargar_datos():
     # Función para cargar datos de la base de datos
-    query="SELECT id, cod_pedido, fecha_pedido, periodo, (CASE customers.alias WHEN null THEN adquiriente ELSE customers.alias END) AS adquiriente, importe_total, rubro, promedio_factura, contado_credito, bancariza, notas, estado, punto_entrega FROM pedidos AS a JOIN customers ON customers.ruc=a.adquiriente"
+    query = "SELECT id, cod_pedido, fecha_pedido, periodo, (CASE customers.alias WHEN null THEN adquiriente ELSE customers.alias END) AS adquiriente, importe_total, rubro, promedio_factura, contado_credito, bancariza, notas, estado, punto_entrega FROM pedidos AS a JOIN customers ON customers.ruc=a.adquiriente"
     #REVISAR CONSULTA POR INDICES DUPLICADOS, O HACER RESET_INDEX(DROP=TRUE)
     return pd.read_sql(query, salessystem)
+
 
 cotizaciones = pd.read_sql("SELECT * FROM facturas WHERE estado NOT IN ('TERMINADO', 'ENTREGADO', 'ANULADA')",
                            salessystem)
@@ -32,17 +33,17 @@ cotizaciones_poremitir = cotizaciones[~cotizaciones['estado'].isin(['TERMINADO',
 
 facturas_poremitir = (cotizaciones_poremitir.groupby(['cod_pedido', 'cuo'])
                       .agg({
-                        'alias': 'first',
-                        'emision': 'first',
-                        'ruc': 'first',
-                        'nombre_razon': 'first',
-                        'moneda': 'first',
-                        'precio_unit': lambda x: (x * cotizaciones_poremitir.loc[x.index, 'cantidad']).sum() * 1.18,
-                        'forma_pago': 'first',
-                        'observaciones': 'first',
-                        'detraccion': 'first',
-                        'retencion': 'first',
-                        'estado': 'first'
+    'alias': 'first',
+    'emision': 'first',
+    'ruc': 'first',
+    'nombre_razon': 'first',
+    'moneda': 'first',
+    'precio_unit': lambda x: (x * cotizaciones_poremitir.loc[x.index, 'cantidad']).sum() * 1.18,
+    'forma_pago': 'first',
+    'observaciones': 'first',
+    'detraccion': 'first',
+    'retencion': 'first',
+    'estado': 'first'
 }).reset_index())
 
 # Renombramos la columna de precio_unit para que tenga el nombre correcto si es necesario
