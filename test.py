e26a7ce8332907e0d85ccba772e90d0d145044e6
Raknaros <giumarchand@gmail.com>
2024-11-21 15:21:39 -0500

    first in dev

--- test.py
+++ test.py
@@ -1,42 +1,58 @@
+import pandas as pd
+from datetime import date
+
+from sqlalchemy import create_engine
 import numpy as np
 
-from models import *
-from sqlalchemy import create_engine
-import pandas as pd
-import streamlit_authenticator as stauth
+from services.Querys import salessystem, pedidos, adquirientes
 from streamlit_authenticator.utilities.hasher import Hasher
-
-from services.PutPedidos import put_pedidos
-from services.Querys import cargar_datos
 
 passwords_to_hash = ['emisor2024', 'EvelynCBM1968', 'p259E9C695+']
 #hashed_passwords = Hasher(passwords_to_hash).generate()
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 
-pedidos = pd.read_excel('D:/OneDrive/facturacion/importar.xlsx', sheet_name='pedidos', date_format='%d/%m/%Y',
-                                dtype={'periodo': np.int32, 'adquiriente': object, 'importe_total': np.int64,
-                                       'rubro': str,
-                                       'promedio_factura': None, 'contado_credito': str,
-                                       'punto_entrega': str, 'notas': str, 'estado': str},
-                                parse_dates=[0, ],
-                                na_values=' ', false_values=['no', 'NO', 'No'], true_values=['si', 'Si', 'SI'])
-
-str_columns = ['rubro', 'contado_credito', 'punto_entrega', 'notas', 'estado']
-for column in str_columns:
-    if pedidos[column].notna().any():
-        pedidos[column] = pedidos[column].apply(lambda x: x.strip().upper() if pd.notna(x) else x)
-pedidos.replace(np.nan, None, inplace=True)
+def get_precuadros(cod_pedidos):
 
 
-print(put_pedidos(pedidos.to_dict(orient='records')))
-"""if authentication_status is False or authentication_status is None:
-    st.error("Por favor, inicie sesión para acceder a esta página.")
-    # Aquí podrías redirigir a la página de inicio de sesión o mostrar un formulario de inicio de sesión
-    # Por ejemplo, puedes mostrar el formulario de autenticación:
-    sleep(2)
-    st.switch_page("home.py")
-"""
+    pedidos_pendientes = pd.merge(pedidos, adquirientes[['ruc', 'alias']], left_on='adquiriente', right_on='ruc', how='left').query('estado == PENDIENTE')
+
+    encabezado = ['cuo', 'alias', 'emision', 'descripcion', 'cantidad', 'precio_unit', 'total',
+                  'peso_articulo', 'peso_total', 'observaciones', 'vencimiento', 'cuota1', 'vencimiento2',
+                  'cuota2', 'vencimiento3', 'cuota3', 'vencimiento4', 'cuota4', 'moneda',
+                  'unid_medida', 'traslado', 'lugar_entrega', 'placa', 'conductor', 'datos_adicionales']
+
+    with pd.ExcelWriter('pedidos_' + date.today().strftime('%Y%m%d') + '.xlsx', engine='xlsxwriter') as writer:
+        for cod_pedido in pedidos_pendientes['cod_pedido'].tolist():
+            try:
+                workbook = writer.book
+                current_worksheet = workbook.add_worksheet(cod_pedido)
+                current_worksheet.write_row(0, 0,
+                                            pedidos.loc[pedidos['cod_pedido'] == cod_pedido].values.flatten().tolist())
+                current_worksheet.write_column()
+            except:
+                None
+            finally:
+                current_worksheet.write_row(1, 0, encabezado)
+                cell_format1 = workbook.add_format({'bold': True, 'font_size': 12})
+                cell_format2 = workbook.add_format({'bold': False, 'font_size': 10})
+                current_worksheet.set_row(0, None, cell_format1)
+                current_worksheet.set_row(1, None, cell_format1)
+                current_worksheet.set_column(0, 0, 7, cell_format2)
+                current_worksheet.set_column(1, 1, 9, cell_format2)
+                current_worksheet.set_column(2, 2, 11, cell_format2)
+                current_worksheet.set_column(3, 3, 45, cell_format2)
+                current_worksheet.write('G3', 'ROUND(E3*F3*1.18;3)')
+                current_worksheet.write('I3', 'ROUNDUP(E3*H3;0)')
+
+
+
+    return print('pedidos_' + date.today().strftime('%Y%m%d') + ' generado')
+
+
+get_precuadros()
 
 
 #TODO en cargar_pedidos revisar que al ingresar un pedido de un adquiriente nuevo aparezca en la tabla
