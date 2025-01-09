43ad3014b52995860d5f0d63225fedf6e879fff8
Raknaros <giumarchand@gmail.com>
2025-01-08 21:22:23 -0500

    first in dev

--- pages/2_cargar_pedidos.py
+++ pages/2_cargar_pedidos.py
@@ -7,6 +7,7 @@ from yaml.loader import SafeLoader
 from time import sleep
 
 from services.PutPedidos import put_pedidos
+from services.GetPrecuadro import get_precuadros
 from services.Querys import pedidos, cargar_datos
 
 st.set_page_config(page_title="Pedidos", page_icon=":material/edit:", layout="wide")
@@ -77,8 +78,46 @@ if st.session_state.get("authentication_status"):
     st.header("Ingresar Pedido")
 
     with st.form(key='load_pedidos', border=False, clear_on_submit=True):
-        col1, col2, col3 = st.columns(3)
 
+        row1 = st.columns(4)
+        row2 = st.columns(4)
+        row3 = st.columns(3)
+
+        row4 = st.container(height=50, border=False)
+
+
+        fecha_pedido = row1[0].date_input('Fecha del pedido',
+                                          help='Indicar la fecha en la que fue solicitado el pedido')
+        periodo = row1[1].text_input('Periodo', placeholder='Ejem: 202407 o 202309',
+                                     help='Indicar a que periodo corresponde el pedido')
+        adquiriente = row1[2].text_input('Adquiriente', placeholder='Ejem: 20131312955 o ALIAS',
+                                         help='Indicar el ruc o el alias de la empresa a la que se le facturara')
+        total = row1[3].text_input('Total', placeholder='Ejem: 400000', help='Indicar total incluido igv del pedido')
+        rubro = row2[0].text_input('Rubro', placeholder='Ejem: MINERO, FERRETERO, EMBALAJE, DIVERSO',
+                                   help='Indicar a que rubro o de que categoria son los articulos del pedido')
+        promedio = row2[1].text_input('Promedio', placeholder='Ejm: 12000 o 5000 o 2000',
+                                      help='Total promedio por factura')
+        punto_llegada = row2[2].text_input('Punto de llegada',
+                                           placeholder='Ejm: DE.DEPOSITO 0014 o CAR.CENTRAL KM. 8.6 LIMA - LIMA - ATE',
+                                           help='Indicar punto de llegada para consignar en las guias, ya sea direccion, '
+                                                'o codigo de establecimiento anexo')
+        forma_pago = row2[3].container(height=97, border=False).text_input('Tipo de pago',
+                                                                           placeholder='Ejm: CONTADO o CREDITO MIN 3 DIAS',
+                                                                           help='Indicar si el pago a consignar en las '
+                                                                                'facturas sera al contado o a credito, si es '
+                                                                                'credito indicar a cuantos dias')
+
+        notas = row3[0].text_area('Notas',
+                                  placeholder='Ejm: CLIENTE BANCARIZA, EVITAR PROVEEDORES A Y B, ENTREGAR MAXIMO EL DIA XX',
+                                  height=100, help='Indicar detalles adicionales o solicitudes puntuales del cliente')
+
+        masivo = row3[1].file_uploader("Subir Masivo", type=['xlsx'],
+                                       help='Subir archivo excel para ingreso de varios pedidos, si sube excel no ingresar '
+                                            'otros datos.')
+
+        submit = row3[2].form_submit_button('Subir')
+        """
+        col1, col2, col3, col4 = st.columns(4)
         fecha_pedido = col1.date_input('Fecha del pedido', help='Indicar la fecha en la que fue solicitado el pedido')
         periodo = col2.text_input('Periodo', placeholder='Ejem: 202407 o 202309',
                                   help='Indicar a que periodo corresponde el pedido')
@@ -106,7 +145,7 @@ if st.session_state.get("authentication_status"):
                                     help='Subir archivo excel para ingreso de varios pedidos, si sube excel no ingresar '
                                          'otros datos.')
         submit = col2.form_submit_button('Subir')
-
+"""
     if submit:
         if masivo is not None:
             pedidos = pd.read_excel(masivo, sheet_name='pedidos', date_format='%d/%m/%Y',
@@ -145,5 +184,6 @@ else:
     st.error("Por favor inicia sesion para continuar...")
     sleep(2)
     st.switch_page("app.py")
+
 
 #TODO AGREGAR BOTON PARA DESCARGAR PRE CUADRO
--- services/GetPrecuadro.py
+++ services/GetPrecuadro.py
@@ -1,32 +1,53 @@
+from io import BytesIO
+
 import pandas as pd
 from datetime import date
 
 from sqlalchemy import create_engine
 import numpy as np
 
-from services.Querys import salessystem, pedidos, adquirientes
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+from services.Querys import salessystem, pedidos, adquirientes, pre_detalle, catalogo
 
 
-def get_precuadros():
 
-    pedidos_alias = pd.merge(pedidos, adquirientes[['ruc', 'alias']], left_on='adquiriente', right_on='ruc', how='left')
+
+
+
+def get_precuadros(ped_seleccionados):
+    if ped_seleccionados == 'Todos':
+        ped_seleccionados = pedidos.loc[pedidos['estado'] == 'PENDIENTE']['cod_pedido'].tolist()
+    #else:
+    #    seleccionados = [x.strip().upper() for x in ped_seleccionados.split(',')]
+
+    pedidos_extendido = pd.merge(pedidos, adquirientes[['ruc', 'alias']], left_on='adquiriente', right_on='ruc',
+                                 how='left')
 
     encabezado = ['cuo', 'alias', 'emision', 'descripcion', 'cantidad', 'precio_unit', 'total',
                   'peso_articulo', 'peso_total', 'observaciones', 'vencimiento', 'cuota1', 'vencimiento2',
                   'cuota2', 'vencimiento3', 'cuota3', 'vencimiento4', 'cuota4', 'moneda',
                   'unid_medida', 'traslado', 'lugar_entrega', 'placa', 'conductor', 'datos_adicionales']
 
-    with pd.ExcelWriter('pedidos_' + date.today().strftime('%Y%m%d') + '.xlsx', engine='xlsxwriter') as writer:
-        for cod_pedido in pedidos['cod_pedido'].tolist():
+    detalle_completo = pd.merge(pre_detalle, catalogo, on='descripcion', how='left')
+
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        for cod_pedido in ped_seleccionados:
+            detalle = detalle_completo.loc[detalle_completo['numero_documento'] == str(int(pedidos_extendido.loc[pedidos_extendido['cod_pedido'] == cod_pedido]['ruc'].values.item()))].sort_values(by='fecha_emision', ascending=False).head(30)
             try:
                 workbook = writer.book
                 current_worksheet = workbook.add_worksheet(cod_pedido)
+                #['cod_pedido','periodo','importe_total', 'rubro', 'promedio_factura', 'contado_credito''notas' 'punto_entrega''alias']
                 current_worksheet.write_row(0, 0,
-                                            pedidos.loc[pedidos['cod_pedido'] == cod_pedido].values.flatten().tolist())
-                current_worksheet.write_column()
+                                            pedidos_extendido[
+                                                ['cod_pedido', 'periodo', 'alias', 'contado_credito', 'importe_total',
+                                                 'promedio_factura', 'notas', 'rubro',
+                                                 'punto_entrega', 'ruc']].loc[pedidos_extendido['cod_pedido'] == cod_pedido].values.flatten().tolist())
+                current_worksheet.write_column(2, 3, detalle['descripcion'].tolist()) #descripcion
+                current_worksheet.write_column(2, 4, detalle['cantidad'].tolist()) #cantidad
+                current_worksheet.write_column(2, 5, detalle['precio_unitario'].tolist()) #precio
+                current_worksheet.write_column(2, 7, detalle['peso'].tolist()) #peso
+                current_worksheet.write_column(2, 19, detalle['unidad_medida_y'].tolist()) #unidad_medida
+
             except:
                 None
             finally:
@@ -42,9 +63,11 @@ def get_precuadros():
                 current_worksheet.write('G3', 'ROUND(E3*F3*1.18;3)')
                 current_worksheet.write('I3', 'ROUNDUP(E3*H3;0)')
 
+        processed_data = output.getvalue()
+
+    return processed_data
 
 
-    return print('pedidos_' + date.today().strftime('%Y%m%d') + ' generado')
 
 
-get_precuadros()
+#TODO en cargar_pedidos revisar que al ingresar un pedido de un adquiriente nuevo aparezca en la tabla
